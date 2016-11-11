c54b4692ed6f8d2c8eccd400ed34b163ceb40c8f
gfike19 <gfike19@gmail.com>
2016-11-10 20:04:11 -0600

    did the persistence video

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,8 +1,12 @@
 package org.launchcode.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.launchcode.models.HelloLog;
 import org.launchcode.models.HelloMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private HelloLogDao helloLogDao;
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloForm () {
 		return "helloform";
@@ -23,11 +30,20 @@ public class HelloController {
 		// get name parameter from request; will be null of no parameter passed in
 		String name = request.getParameter("name");
 		
-		if (name == null) {
+		if (name == null || name == "") {
 			name = "world";
 		}
+		HelloLog log = new HelloLog(name);
+		helloLogDao.save(log);
 		model.addAttribute("message", HelloMessage.getMessage(name));
 		return "hello"; //if having folders, put in the path name
+	}
+	
+	@RequestMapping(value = "/log")
+	public String log(Model model) {
+		List<HelloLog> logs = helloLogDao.findAll();
+		model.addAttribute("logs", logs);
+		return "log";
 	}
 	
 }
